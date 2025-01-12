f39b5ac3ff0857cae2b54b23175b45d139ae20c8
Artem Pugach <pugacartem106@gmail.com>
2025-01-12 21:00:51 +0200

    Updated FuncA to sum first 3 terms of series

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,7 +1,14 @@
 #include "FuncA.h"
-#include <cmath>
+#include <cmath>  
+
 
 double FuncA::FuncA(double x) {
-    return 0;  
+    double result = 0;  // Ініціалізуємо змінну для зберігання результату
+    // Обчислюємо суму перших 3 елементів
+    for (int i = 0; i < 3; ++i) {
+        // Додаємо до результату поточний елемент послідовності
+        result += std::pow(-1, i) * std::pow(x, 2 * i + 1) / (2 * i + 1);
+    }
+    return result;  // Повертаємо обчислений результат
 }
 
