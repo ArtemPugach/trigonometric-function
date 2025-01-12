1bd9f51d0ab92dfa195f5d69567a9f1275c97e03
Artem Pugach <pugacartem106@gmail.com>
2025-01-12 20:56:46 +0200

    Updated FuncA to sum first n terms of series

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,7 +1,11 @@
 #include "FuncA.h"
 #include <cmath>
 
-double FuncA::FuncA(double x) {
-    return 0;  
+double FuncA::FuncA(double x, int n) {
+    double result = 0;
+    for (int i = 0; i < n; ++i) {
+        result += std::pow(-1, i) * std::pow(x, 2 * i + 1) / (2 * i + 1);
+    }
+    return result;
 }
 
